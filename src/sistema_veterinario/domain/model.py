50bd859847d81dca7370434f72b148e701b06c05
Vt-dnts <carlosvictorsds@gmail.com>
2026-09-15 17:31:56 -0300

    feat: adiciona entidade Veterinario e objeto Disponibilidade

--- src/sistema_veterinario/domain/model.py
+++ src/sistema_veterinario/domain/model.py
@@ -1,3 +1,21 @@
+from dataclasses import dataclass
+from datetime import time
+
+@dataclass(frozen=True)
+class Disponibilidade:
+   inicio: time
+   fim: time
+
+   def _post_init_(self):
+       if self.inicio >= self.fim:
+           raise ValueError("O horário de início deve ser menor do que o horário final")
+
+class Veterinario:
+   def _init_(self, id_veterinario, nome, crmv):
+       self.id_veterinario = id_veterinario
+       self.nome = nome
+       self.crmv = crmv
+
 class Agendamento:
     def __init__(self, id_agendamento, id_cliente, id_paciente, id_veterinario, data_hora):
         self.id_agendamento = id_agendamento
