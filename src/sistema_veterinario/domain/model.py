09a81c993f11506ca0e3537978b688824d3974da
Davi Gesteira <daviges@gmail.com>
2026-09-14 23:05:40 -0300

    feat: cria entidade inicial de agendamento

---
+++ src/sistema_veterinario/domain/model.py
@@ -0,0 +1,7 @@
+class Agendamento:
+    def __init__(self, id_agendamento, id_cliente, id_paciente, id_veterinario, data_hora):
+        self.id_agendamento = id_agendamento
+        self.id_cliente = id_cliente
+        self.id_paciente = id_paciente
+        self.id_veterinario = id_veterinario
+        self.data_hora = data_hora
