587193510c5d47e973603609e03b9a2c07344f8a
Davi Gesteira <daviges@gmail.com>
2026-09-15 22:11:01 -0300

    feat: adiciona regras de status e validação de data do agendamento

--- src/sistema_veterinario/domain/model.py
+++ src/sistema_veterinario/domain/model.py
@@ -1,25 +1,57 @@
 from dataclasses import dataclass
-from datetime import time
+
 
 @dataclass(frozen=True)
 class Disponibilidade:
-   inicio: time
-   fim: time
+    inicio: time
+    fim: time
 
-   def _post_init_(self):
-       if self.inicio >= self.fim:
-           raise ValueError("O horário de início deve ser menor do que o horário final")
+    def __post_init__(self):
+        if self.inicio >= self.fim:
+            raise ValueError(
+                "O horário de início deve ser menor do que o horário final"
+            )
+
 
 class Veterinario:
-   def _init_(self, id_veterinario, nome, crmv):
-       self.id_veterinario = id_veterinario
-       self.nome = nome
-       self.crmv = crmv
+    def __init__(self, id_veterinario, nome, crmv):
+        self.id_veterinario = id_veterinario
+        self.nome = nome
+        self.crmv = crmv
+from datetime import datetime
+
 
 class Agendamento:
     def __init__(self, id_agendamento, id_cliente, id_paciente, id_veterinario, data_hora):
+        if data_hora <= datetime.now():
+            raise ValueError("A data do agendamento deve ser futura")
+
         self.id_agendamento = id_agendamento
         self.id_cliente = id_cliente
         self.id_paciente = id_paciente
         self.id_veterinario = id_veterinario
         self.data_hora = data_hora
+        self.status = "AGENDADO"
+
+    def confirmar(self):
+        if self.status != "AGENDADO":
+            raise ValueError(
+                "O agendamento não pode ser confirmado, pois não está no status AGENDADO"
+            )
+        self.status = "CONFIRMADO"
+
+
+    def cancelar(self):
+        if self.status == "AGENDADO":
+            self.status = "CANCELADO"
+        else:
+            raise ValueError(
+                "O agendamento não pode ser cancelado, pois não está no status AGENDADO"
+            )
+            
+    def concluir(self):
+        if self.status != "CONFIRMADO":
+            raise ValueError(
+                "O agendamento não pode ser concluído, pois não está no status CONFIRMADO"
+            )
+        self.status = "CONCLUIDO"
